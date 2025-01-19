feafecac6870b9232a8461bf00eb3288a6cad853
Mike Kraus <mike.kraus@mollie.com>
2025-01-19 14:21:28 +0100

    feat: make hidden files visible

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("neo-tree").setup({
+    filesystem = {
+      filtered_items = {
+        visible = true, -- This makes sure all files are visible, including hidden ones
+        hide_dotfiles = false, -- Set this to false to show dotfiles (hidden files)
+      },
+    },
+  })
