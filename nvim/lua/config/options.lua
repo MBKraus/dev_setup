87dbba4d1f7fbf56f52fe9925fcadc9818e92a60
Mike Kraus <mike.kraus@mollie.com>
2025-02-26 11:21:27 +0100

    add clipboard setup

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,19 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_picker = "telescope"
+
+-- https://gaganpreet.in/posts/tmux-ssh-remote-clipboard/
+vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+  
+  vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
+  
